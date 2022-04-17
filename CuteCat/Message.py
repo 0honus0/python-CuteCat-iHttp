f0bc16650afab9cd313d812dd47669f2709cbc07
0honus0 <63273720+0honus0@users.noreply.github.com>
2022-04-17 13:53:57 +0800

    add type 0

--- CuteCat/Message.py
+++ CuteCat/Message.py
@@ -7,6 +7,7 @@ class Message:
     @property
     def type(self):
         type_dict = {
+            0      : 'eventnotify', # 成员变更 面对面付款
             1      : 'text',
             3      : 'image',
             34     : 'voice',
@@ -23,7 +24,7 @@ class Message:
             2003   : 'groupinvite',
             2005   : 'revokemsg',
             2006   : 'groupannouncement',
-            10000  : 'sysmsg',      # sysmsg , 包含 拍一拍 语音消息 撤回消息 等等 , 通过SendOutMsg接收
+            10000  : 'sysmsg',      # 拍一拍 语音消息 撤回消息 等等 , 通过SendOutMsg接收
             10002  : 'other'        # multivoip , taptap , ClientCheckConsistency
         }
         return type_dict[self.msg['type']] if self.msg['type'] in type_dict else self.msg['type']
